5fc2e774c22d7153060a39e2b8a509fd81a6436c
_JXc <hsvbuh12@gmail.com>
2023-05-02 18:58:59 +0200

    Inital Upload

---
+++ src/main/java/de/epochtraveler/template/EpochCamModule.java
@@ -0,0 +1,26 @@
+package de.epochtraveler.template;
+
+import de.epochtraveler.epochapi.CoreServer;
+import de.epochtraveler.epochapi.modules.Module;
+import de.epochtraveler.epochapi.modules.ModuleDescription;
+
+@ModuleDescription(
+        name = "EpochCam-Module",
+        version = "1.0.0"
+)
+public final class EpochCamModule extends Module {
+
+    public EpochCamModule(CoreServer server) {
+        super(server);
+    }
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+}
